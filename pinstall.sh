7258de1b97194dca075d500327990baebc65d53f
progteamlub <69053955+progteamlub@users.noreply.github.com>
2020-08-03 06:40:51 +0500

    Add files via upload

---
+++ pinstall.sh
@@ -0,0 +1,12 @@
+echo HI
+pkg install python
+pip install --upgrade pip
+pip install requests
+pip install os
+pip install sys
+pip install random
+pip install re
+pip install threading
+pip install BeautifulSoup
+pip install from
+python PTLDoS.py
